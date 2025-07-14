45e40bac21224ddd8ff0717eca3725b508ac98cc
damian-szymanski <damian.szymanski613@gmail.com>
2025-07-14 11:29:39 +0200

    Delete app/main.py

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, Query
-from rag_chatbot import answer_question 
-
-app = FastAPI()
-
-@app.get("/ask")
-def ask(question: str = Query(..., description="Pytanie do systemu")):
-    answer = answer_question(question)
-    return {"odpowiedź": answer}
