2f13b0b3051893e4477da29e59db1600b99f5abe
damian-szymanski <damian.szymanski613@gmail.com>
2025-07-14 11:27:22 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
+from fastapi import FastAPI, Query
+from rag_chatbot import answer_question 
 
+app = FastAPI()
+
+@app.get("/ask")
+def ask(question: str = Query(..., description="Pytanie do systemu")):
+    answer = answer_question(question)
+    return {"odpowiedź": answer}
